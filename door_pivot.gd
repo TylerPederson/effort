611214e78803d61ce69d1f9776548339c2b0992e
EdgarAlamillo <edgar.alamillo805@gmail.com>
2026-02-18 09:43:15 -0800

    Setting up area for door trigger

--- door_pivot.gd
+++ door_pivot.gd
@@ -1,17 +1,15 @@
 extends Node3D
 
-var speed = 1
+var speed = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _drop_door(delta):	# Function rotates the door to drop down
-	while rotation_degrees.z < 90:
+	if rotation_degrees.z < 90:
 		rotation_degrees.z += speed * delta
-		if rotation_degrees.z > 90:
-			break		
-	pass			
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	_drop_door(delta)
