1d9589a99a5a8f16a9b7960d763fecbc720f0eed
EdgarAlamillo <edgar.alamillo805@gmail.com>
2026-02-18 10:58:33 -0800

    Added a door trigger to open it

--- door_pivot.gd
+++ door_pivot.gd
@@ -1,15 +1,20 @@
 extends Node3D
 
-var speed = 5
+var speed = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _drop_door(delta):	# Function rotates the door to drop down
-	if rotation_degrees.z < 90:
+	while rotation_degrees.z < 90:
 		rotation_degrees.z += speed * delta
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_drop_door(delta)
+	pass
+
+
+func _on_area_3d_body_entered(body):
+	#print_debug("Entered")
+	_drop_door(get_physics_process_delta_time())
