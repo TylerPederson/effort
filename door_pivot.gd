4e93ebceec82f154dac03b5f1a2b121ae4a5a87e
EdgarAlamillo <edgar.alamillo805@gmail.com>
2026-02-19 08:32:44 -0800

    door dropping is now smoother

--- door_pivot.gd
+++ door_pivot.gd
@@ -1,20 +1,23 @@
 extends Node3D
 
-var speed = 1
+var speed = 5
+var drop = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _drop_door(delta):	# Function rotates the door to drop down
-	while rotation_degrees.z < 90:
-		rotation_degrees.z += speed * delta
-		
+func _drop_door(drop: bool):	# Function rotates the door to drop down
+	if drop == true:
+		if rotation_degrees.z < 90:
+			rotation_degrees.z += speed * get_physics_process_delta_time()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	_drop_door(drop)
 
 
 func _on_area_3d_body_entered(body):
 	#print_debug("Entered")
-	_drop_door(get_physics_process_delta_time())
+	drop = true
+	
