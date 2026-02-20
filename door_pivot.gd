9961df7ce9925c3411c9746bfbc9c989d569d8b0
EdgarAlamillo <edgar.alamillo805@gmail.com>
2026-02-20 09:54:32 -0800

    Added lift door trigger and cleaned up some code

--- door_pivot.gd
+++ door_pivot.gd
@@ -1,23 +1,40 @@
 extends Node3D
 
-var speed = 5
+var speed = 7
 var drop = false
+var lift = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _drop_door(drop: bool):	# Function rotates the door to drop down
+func _drop_door(delta: float):	# Function rotates the door to drop down
 	if drop == true:
 		if rotation_degrees.z < 90:
-			rotation_degrees.z += speed * get_physics_process_delta_time()
+			rotation_degrees.z += speed * delta
+			if rotation_degrees.z >= 90:	# Makes sure not to go all the way through the ground 
+				drop = false
 	
+func _lift_door(delta: float): # Rotates the door to lift after played enters arena 
+	if lift == true:
+		#print_debug("Lift is true")
+		if rotation_degrees.z >= 0:
+			rotation_degrees.z -= speed * delta
+			if rotation_degrees.z <= 0:	# Makes sure not to go backwards
+				lift = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_drop_door(drop)
+	_drop_door(delta)
+	_lift_door(delta)
 
-
-func _on_area_3d_body_entered(body):
+func _on_area_3d_body_entered(body): # Activates when player enters trigger 
 	#print_debug("Entered")
-	drop = true
+	if body.name == "Player":
+		drop = true
 	
+
+
+func _on_area_3d_2_body_exited(body):
+	#print_debug("Exited")
+	if body.name == "Player":
+		lift = true
