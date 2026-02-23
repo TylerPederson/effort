c26b8c93eea7b36b21a55df5d0ff62535817fab9
EdgarAlamillo <edgar.alamillo805@gmail.com>
2026-02-23 14:25:50 -0800

    Organized files

--- Boss/BossStates/StateMachine.gd
+++ Boss/BossStates/StateMachine.gd
@@ -1,10 +1,10 @@
 extends Node
 
 var state = {
-	"Idle" = preload("res://BossStates/IdleState.gd"),
-	"Walk" = preload("res://BossStates/WalkState.gd"),
-	"Attack" = preload("res://BossStates/AttackState.gd"),
-	"Death" = preload("res://BossStates/DeathState.gd")
+	"Idle" = preload("res://Boss/BossStates/IdleState.gd"),
+	"Walk" = preload("res://Boss/BossStates/WalkState.gd"),
+	"Attack" = preload("res://Boss/BossStates/AttackState.gd"),
+	"Death" = preload("res://Boss/BossStates/DeathState.gd")
 	}
 func _change_state(newState: String):
 	if get_child_count() != 0:
