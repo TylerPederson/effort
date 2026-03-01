dfd6bc60010387ce6dd9c6afacc645e3f73aa29b
Tyler Pederson <tyler.lee.pederson@gmail.com>
2026-03-01 14:50:32 -0800

    Add Damaging, Health, and hitbox components
    This will allow anything to have simple health, hitbox, and damage component. The sandbox scene included contains spikes that have a damage component and a player that has health and hitbox components. The spikes will damage the player when they collide.

---
+++ stats-and-combat/Components/damage_component.gd
@@ -0,0 +1,32 @@
+extends Area3D
+class_name DamageComponent
+
+@export var damage_value : int = 10
+@export var cooldown : float = 1.0
+var ready_to_damage: bool = true
+
+var cooldown_timer: Timer
+
+func _ready():
+	connect("area_entered", _on_area_entered)
+	cooldown_timer = $CooldownTimer
+	cooldown_timer.wait_time = cooldown
+	cooldown_timer.one_shot = true
+	cooldown_timer.autostart = false
+	cooldown_timer.connect("timeout", on_cooldown_timer_timeout)
+
+
+func _on_area_entered(body):
+	if body is not HitboxComponent:
+		return
+	if not ready_to_damage:
+		return
+	
+	var hitbox : HitboxComponent = body
+	hitbox.receive_damage(damage_value)
+	
+	ready_to_damage = false
+	cooldown_timer.start()
+
+func on_cooldown_timer_timeout():
+	ready_to_damage = true
