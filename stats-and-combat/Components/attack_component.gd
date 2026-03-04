f1a13fe784f5a8a1f2d86710e4407e7ce352fe0c
Tyler Pederson <tyler.lee.pederson@gmail.com>
2026-03-04 14:24:38 -0800

    Add helper functions to attack_component
    Attack component can be set to auto_attack enabled or disabled with a helper function. It also has a _on_attack_action function to be targeted by signals from objects with this component.

--- stats-and-combat/Components/attack_component.gd
+++ stats-and-combat/Components/attack_component.gd
@@ -1,20 +1,25 @@
 extends Node3D
 class_name AttackComponent
 
+# All objects that can attack expect there to be a weapon_holder node that
+# holds the transform where the weapon will be at. The weapon_holder should
+# have a WeaponComponent child.
 @export var weapon_holder: Node3D
 
 var _equipped_weapon: WeaponComponent = null
 var _attacking : bool = false
-var _auto_attack : bool = true
+var _auto_attack : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_refresh_weapon()
 
+# The attack_component can attempt to attack constantly if auto attack is enabled
 func _process(delta: float) -> void:
 	if _auto_attack:
 		_try_attack()
 
+# Grabs the expected weapon component from the weapon_holder.
 func _refresh_weapon():
 	if weapon_holder == null:
 		return
@@ -23,6 +28,7 @@ func _refresh_weapon():
 			_equipped_weapon = c
 			break
 
+# If a weapon component is available, then attempt to use it in an attack
 func _try_attack():
 	if _attacking:
 		return
@@ -38,3 +44,9 @@ func _try_attack():
 	await _equipped_weapon.attack_finished
 	
 	_attacking = false
+
+func set_auto_attack(enable: bool):
+	_auto_attack = enable
+
+func _on_attack_action():
+	_try_attack()
