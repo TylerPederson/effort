9b975af97683437153ee9a142a0c9a319830383e
Tyler Pederson <tyler.lee.pederson@gmail.com>
2026-03-18 17:53:19 -0700

    Enemies can be added to managers
    The enemy manager will track all enemies that are assigned to it from the editor. When all enemies have been destroyed, a signal will be sent. Any node that wants to know when all enemies in a room have been defeated will be alerted.

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -5,17 +5,35 @@ enum EnemyType {
 	Ranged
 }
 
+signal died
+
+# Expected enemy manager to be set so that a level or other custom logic can
+# be completed when all enemies in a location die
+@export var manager : EnemyManager
+
+# Enemy type alters movement and attack type
 @export var type : EnemyType
+
+# Basic variables that any enemy will have
 @export var move_speed : float = 100.0
 @export var health : int = 30
 @export var damage : int = 5
 @export var attack_range : float = 2.0
 @export var attack_cooldown : float = 1.5
 
+# Variables used for range enemies to only move to a safe distance
+var closest_chase_dist := 36.0
+var closest_backup_dist := 16.0
+var backoff_speed_multiplier := 2.0
+
 var player: Node3D
 var following : bool = false
 
+# Set all stats from editor
 func _ready() -> void:
+	if not manager == null:
+		manager.link(self)
+	
 	%HealthComponent.max_hp = health
 	%HealthComponent.current_hp = health
 	
@@ -23,18 +41,22 @@ func _ready() -> void:
 	%AttackComponent._equipped_weapon.cooldown = attack_cooldown
 	%AttackComponent._equipped_weapon.attack_range = attack_range
 	
-	
 	if type == EnemyType.Melee:
 		%AttackComponent._equipped_weapon.attackStyle = WeaponComponent.WeaponAttackStyle.STAB
 	if type == EnemyType.Ranged:
 		%AttackComponent._equipped_weapon.attackStyle = WeaponComponent.WeaponAttackStyle.SHOOT
 
+
+# Handle any custom behavior logic each frame
 func _process(delta: float) -> void:
 	if following:
+		# Attempt to attack toward player
 		%AttackComponent.set_auto_attack(true)
 		look_at(player.position)
 		rotation.x = 0
 		rotation.z = 0
+		
+		# Any future logic dependent on enemy type would go here
 		if type == EnemyType.Ranged:
 			pass
 		if type == EnemyType.Melee:
@@ -42,21 +64,48 @@ func _process(delta: float) -> void:
 	else:
 		%AttackComponent.set_auto_attack(false)
 
+# Handle movement from gravity and chasing the player
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
 	if following:
 		var direction =  -global_transform.basis.z
 		velocity.x = direction.x * move_speed * delta
 		velocity.z = direction.z * move_speed * delta
 		
-		move_and_slide()
+		# Adds a small chance to jump at any given moment, mostly to
+		# prevent getting permanently stuck on small ledges
+		if randf() < 0.005:
+				velocity.y = 3
 		
+		# Ranged Enemies have a special 'deadzone' where they 
+		# will stop chasing the player or move away quickly if the 
+		# player is too close
 		if type == EnemyType.Ranged:
-			pass
+			var distance = global_position.distance_squared_to(player.global_position)
+			
+			if distance < closest_chase_dist:
+				velocity.z = 0
+				velocity.x = 0
+			
+			if distance < closest_backup_dist:
+				var runaway_speed = move_speed * backoff_speed_multiplier
+				velocity.z = -direction.x * runaway_speed * delta
+				velocity.x = -direction.x * runaway_speed * delta
+		
+		# Any future changes to melee move algorithms would go here
 		if type == EnemyType.Melee:
 			pass
-			
+	# Stop moving if the player is too far away
+	else:
+		velocity.x = 0
+		velocity.z = 0
+	
+	move_and_slide()
 
 func _on_health_component_death() -> void:
+	died.emit()
 	queue_free()
 
 
