42457e6d7964b4b1a27d69292566b308c3067647
EdgarAlamillo <edgar.alamillo805@gmail.com>
2026-04-21 14:05:01 -0700

    Boss now has multiple attacks

--- Boss/BossStates/AttackState.gd
+++ Boss/BossStates/AttackState.gd
@@ -7,7 +7,22 @@ func _ready():	# Waits for current animations to finish then starts attacjs
 	if AIController.Awakening:
 		await AIController.get_node("AnimationTree").animation_finished
 	AIController.attack = true
-	AIController.get_node("AnimationTree").get("parameters/playback").travel("Attack")
+	var rng = RandomNumberGenerator.new()
+	var my_int = rng.randi_range(1, 6) 
+	match my_int:
+		1:
+			AIController.get_node("AnimationTree").get("parameters/playback").travel("Attack")
+		2:
+			AIController.get_node("AnimationTree").get("parameters/playback").travel("Attack 2")
+		3:
+			AIController.get_node("AnimationTree").get("parameters/playback").travel("Attack 3")
+		4:
+			AIController.get_node("AnimationTree").get("parameters/playback").travel("Attack 4")
+		5:
+			AIController.get_node("AnimationTree").get("parameters/playback").travel("Attack 5")
+		6:
+			AIController.get_node("AnimationTree").get("parameters/playback").travel("Attack 6")
+			
 	AIController.look_at(AIController.global_transform.origin + AIController.direction, Vector3.UP)
 func _physics_process(delta: float):
 	if AIController:		# Makes sure boss doesn't slide while attacking
