2f0bdb8621f244fa952a635efa6c9a095014f023
Tyler Pederson <tyler.lee.pederson@gmail.com>
2026-05-01 19:07:29 -0700

    Separate light tween and material tween

--- lantern.gd
+++ lantern.gd
@@ -8,28 +8,25 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 func activate(): 
 	$OmniLight3D.visible = true
 	$OmniLight3D.light_energy = 0
 	
-	var mat = $MeshInstance3D.get_active_material(0)
+	var tween = create_tween()
+	tween.tween_property($OmniLight3D, "light_energy", 3.0, 1.5)
+	
+	var mat = $Ball.get_active_material(0)
 	if mat:
 		mat.emission_enable = true
 		mat.emission = Color(0.812, 0.0, 0.0, 1.0)
-		mat.emission_energy = 10 
-		
-	var tween = create_tween()
-	
-	tween.tween_property($OmniLight3D, "light_energy", 3.0, 1.5)
-	if mat: 
-		tween.parallel().tween_property(mat, "emission_energy", 2.0,1.0)
+		#mat.emission_energy = 50
+		var light_tween = create_tween()
+		light_tween.tween_property(mat, "emission_energy_multiplier", 50.0, 3.0)
 
 func turn_off():
 	$OmniLight3D.visible = false
-	var mat = $MeshInstance3D.get_active_material(0)
+	var mat = $Ball.get_active_material(0)
 	if mat:
 		mat.emission_enable = false 
+		mat.emission_energy = 0
 	
