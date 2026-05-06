9ca98dce3ecd825f7673b7323b6ea53ad1ad61dd
Tyler Pederson <tyler.lee.pederson@gmail.com>
2026-05-05 23:13:05 -0700

    Change Explosion texture

--- stats-and-combat/Weapons/explosion.gd
+++ stats-and-combat/Weapons/explosion.gd
@@ -7,8 +7,8 @@ func _ready():
 
 func trigger():
 	await get_tree().create_timer(0.02).timeout
-	%CPUParticles3D.emitting = true
+	%Particles.emitting = true
 	%OmniLight3D.visible = true
-	await get_tree().create_timer(1.5).timeout
+	await get_tree().create_timer(1.0).timeout
 	%OmniLight3D.visible = false
 	queue_free()
